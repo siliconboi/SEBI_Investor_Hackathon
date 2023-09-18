dbe872e4f33c6347dd7a0f9e21e4cd96b8cf36df
rishitashaw <rishitashaw49@gmail.com>
2023-09-18 12:07:50 +0530

    statement added

--- controller/userController.js
+++ controller/userController.js
@@ -9,37 +9,38 @@ const postActionInfo = asyncHandler(async (req, res) => {
   const username = req?.body?.username; //username passed from frontend
   if (body) {
     const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
+      apiKey: process.env.OPENAI_API_KEY
     });
     const openai = new OpenAIApi(configuration);
     try {
       // console.log(configuration)
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `The following is an action. Return both the category the action falls into among categories of [Investments, Savings, Income, Expenses] and the money:${ body }\n\nCategory: \nMoney:`,
-      temperature: 0,
-      max_tokens: 64,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    });
-    // console.log(response.data.choices[0].text);
-    const remain = response.data.choices[0].text.split("\n")[1];
-    const category = remain.split(", ")[0];
-    const money = remain.split("$")[1];
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `The following is an action. Return both the category the action falls into among categories of [Investments, Savings, Income, Expenses] and the money:${ body }\n\nCategory: \nMoney:`,
+        temperature: 0,
+        max_tokens: 64,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+      });
+      // console.log(response.data.choices[0].text);
+      const remain = response.data.choices[0].text.split("\n")[1];
+      const category = remain.split(", ")[0];
+      const money = remain.split("$")[1];
 
       const user = await User.findOneAndUpdate(
         { credentialKeys: username },
         { $inc: { [category]: money } },
         { new: true }
       );
-      
+
       if (user) {
         await Transaction.create({
           type: category,
           amount: money,
           date: new Date(),
           user: user._id,
+          statement: body
         });
         return res.status(200).json({ category, money });
       }
@@ -114,8 +115,8 @@ async function sendOTPEmail(email, otp) {
       from: '"Rishita Shaw" <rishitashaw00@gmail.com>', // Replace with your email address
       to: email,
       subject: "OTP Verification",
-      text: `Your OTP: ${otp}`,
-      html: `<p>Your OTP: <strong>${otp}</strong></p>`,
+      text: `Your OTP: ${ otp }`,
+      html: `<p>Your OTP: <strong>${ otp }</strong></p>`,
     });
     console.log("Email sent:", info.messageId);
   } catch (error) {
